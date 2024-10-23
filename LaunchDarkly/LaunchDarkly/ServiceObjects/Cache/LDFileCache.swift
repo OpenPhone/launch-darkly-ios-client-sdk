54ac4c5760a68cca7bac9f48ce839e4d6f9bd25f
Alexey Naumov <anaumov91@gmail.com>
2024-10-24 01:13:41 +0600

    feat: [UXP-3578] Add file IO

--- LaunchDarkly/LaunchDarkly/ServiceObjects/Cache/LDFileCache.swift
+++ LaunchDarkly/LaunchDarkly/ServiceObjects/Cache/LDFileCache.swift
@@ -1,18 +1,20 @@
 import Foundation
+import OSLog
 
 public final class LDFileCache: KeyedValueCaching {
 
     private static let instancesLock = NSLock()
     private let cacheKey: String
-    private let inMemoryCache: LDInMemoryCache
+    private let inMemoryCache: KeyedValueCaching
     private let fileQueue = DispatchQueue(label: "ld_file_io", qos: .utility).debouncer()
+    private let logger: OSLog
 
-    public static func builder() -> (String) -> KeyedValueCaching {
-        return { cacheKey in
+    public static func builder() -> LDConfig.CacheFactory {
+        return { logger, cacheKey in
             instancesLock.lock()
             defer { instancesLock.unlock() }
-            let inMemoryCache = LDInMemoryCache.builder()(cacheKey)
-            let cache = LDFileCache(cacheKey: cacheKey, inMemoryCache: inMemoryCache)
+            let inMemoryCache = LDInMemoryCache.builder()(logger, cacheKey)
+            let cache = LDFileCache(cacheKey: cacheKey, inMemoryCache: inMemoryCache, logger: logger)
             if inMemoryCache.data(forKey: "is_initialized") == nil {
                 cache.deserializeFromFile()
                 inMemoryCache.set(Data(), forKey: "is_initialized")
@@ -51,22 +53,67 @@ public final class LDFileCache: KeyedValueCaching {
 
     // MARK: - Internal
 
-    init(cacheKey: String, inMemoryCache: LDInMemoryCache) {
+    init(cacheKey: String, inMemoryCache: KeyedValueCaching, logger: OSLog) {
         self.cacheKey = cacheKey
         self.inMemoryCache = inMemoryCache
-    }
-
-    func deserializeFromFile() {
-
-    }
-
-    func serializeToFile() {
-
+        self.logger = logger
     }
 
     func scheduleSerialization() {
         fileQueue.debounce(interval: .milliseconds(500)) { [weak self] in
             self?.serializeToFile()
         }
+    }
+
+    func serializeToFile() {
+        do {
+            var dictionary: [String: Data] = [:]
+            inMemoryCache.keys().forEach { key in
+                if let data = inMemoryCache.data(forKey: key) {
+                    dictionary[key] = data
+                }
+            }
+            let data = try NSKeyedArchiver.archivedData(withRootObject: dictionary, requiringSecureCoding: true)
+            let url = try pathToFile()
+            try data.write(to: url, options: .atomic)
+        } catch {
+            os_log("%s failed writing cache to file. Error: %s",
+                   log: logger, type: .debug, typeName, String(describing: error))
+        }
+    }
+
+    func deserializeFromFile() {
+        do {
+            let url = try pathToFile()
+            let data = try Data(contentsOf: url)
+            guard let flags = try NSKeyedUnarchiver
+                .unarchivedObject(ofClass: NSDictionary.self, from: data) as? [String: Data]
+            else { throw Error.cannotUnarchiveDictionary }
+            flags.forEach { key, value in
+                inMemoryCache.set(value, forKey: key)
+            }
+        } catch {
+            os_log("%s failed loading cache from file. Error: %s",
+                   log: logger, type: .debug, typeName, String(describing: error))
+        }
+    }
+
+    func pathToFile() throws -> URL {
+        let fileManager = FileManager.default
+        guard let dir = fileManager
+            .urls(for: .libraryDirectory, in: .userDomainMask).first?
+            .appendingPathComponent("ld_cache")
+        else { throw Error.cannotAccessLibraryDirectory }
+        try fileManager.createDirectory(at: dir, withIntermediateDirectories: true)
+        return dir.appendingPathComponent(cacheKey)
+    }
+}
+
+extension LDFileCache: TypeIdentifying { }
+
+private extension LDFileCache {
+    enum Error: Swift.Error {
+        case cannotAccessLibraryDirectory
+        case cannotUnarchiveDictionary
     }
 }
