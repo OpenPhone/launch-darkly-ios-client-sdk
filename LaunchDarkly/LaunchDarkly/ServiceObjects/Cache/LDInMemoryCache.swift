4133d701401c4878a2e6321b31ce3fb077a00895
Alexey Naumov <anaumov91@gmail.com>
2024-10-23 19:51:59 +0600

    feat: [UXP-3578] Add LDInMemoryCache

---
+++ LaunchDarkly/LaunchDarkly/ServiceObjects/Cache/LDInMemoryCache.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+public final class LDInMemoryCache: KeyedValueCaching {
+
+    private static var instances: [String: LDInMemoryCache] = [:]
+    private static let instancesLock = NSLock()
+
+    private var cache: [String: Any] = [:]
+    private var cacheLock = NSLock()
+
+    public static var builder: (String) -> KeyedValueCaching {
+        return { cacheKey in
+            instancesLock.lock()
+            defer { instancesLock.unlock() }
+            if let cache = instances[cacheKey] { return cache }
+            let cache = LDInMemoryCache()
+            instances[cacheKey] = cache
+            return cache
+        }
+    }
+
+    public func set(_ value: Data, forKey: String) {
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        cache[forKey] = value
+    }
+
+    public func data(forKey: String) -> Data? {
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        return cache[forKey] as? Data
+    }
+
+    public func dictionary(forKey: String) -> [String : Any]? {
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        return cache[forKey] as? [String: Any]
+    }
+
+    public func removeObject(forKey: String) {
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        cache.removeValue(forKey: forKey)
+    }
+
+    public func removeAll() {
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        cache.removeAll()
+    }
+
+    public func keys() -> [String] {
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        return Array(cache.keys)
+    }
+}
